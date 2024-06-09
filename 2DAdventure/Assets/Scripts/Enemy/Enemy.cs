dffdd79ba9af088ff9309a63299fdc83d59cbff5
CoffeeKiller <18320210361@163.com>
2024-06-09 16:43:57 +0800

    feat: add hit the wall judgment

--- 2DAdventure/Assets/Scripts/Enemy/Enemy.cs
+++ 2DAdventure/Assets/Scripts/Enemy/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
     protected Rigidbody2D rb;
 
     protected Animator anima;
+
+    private PhysicsCheck physicsCheck;
     
     [Header("基本参数")] 
     // 正常速度
@@ -22,17 +24,36 @@ public class Enemy : MonoBehaviour
     // 方向
     public Vector3 faceDir;
 
+    [Header("计时器")] 
+    public float waitTime;
+
+    public float waitTImeCounter;
+    public bool wait;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anima = GetComponent<Animator>();
-
+        physicsCheck = GetComponent<PhysicsCheck>();
+        
         currentSpeed = normalSpeed;
+        waitTImeCounter = waitTime;
     }
 
     private void Update()
     {
         faceDir = new Vector3(-transform.localScale.x, 0, 0);
+
+        // 判断是否碰撞墙壁且面向墙壁
+        if ((physicsCheck.touchLeftWall && faceDir.x < 0) 
+            || (physicsCheck.touchRightWall && faceDir.x > 0))
+        {
+            // transform.localScale = new Vector3(faceDir.x, 1, 1);
+            wait = true;
+            anima.SetBool("walk", false);
+        }
+        
+        TimeCounter();
     }
 
     private void FixedUpdate()
@@ -43,5 +64,20 @@ public class Enemy : MonoBehaviour
     public virtual void Move()
     {
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void TimeCounter()
+    {
+        // 转身计时
+        if (wait)
+        {
+            waitTImeCounter -= Time.deltaTime;
+            if (waitTImeCounter <= 0)
+            {
+                wait = false;
+                waitTImeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
     }
 }
--- 2DAdventure/Assets/Scripts/General/PhysicsCheck.cs
+++ 2DAdventure/Assets/Scripts/General/PhysicsCheck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,9 +7,14 @@ using UnityEngine;
 // 物理碰撞检查组件
 public class PhysicsCheck : MonoBehaviour
 {
-    [Header("检测参数")]
+    private CapsuleCollider2D coll;
+    
+    [Header("检测参数")] 
+    public bool manual;
     // 底部碰撞检测偏移值
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
 
     // 碰撞检测范围
     public float checkRaduis;
@@ -20,6 +26,21 @@ public class PhysicsCheck : MonoBehaviour
     // 是否处于地面状态
     public bool isGround;
 
+    public bool touchLeftWall;
+    
+    public bool touchRightWall;
+
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+
+        if (!manual)
+        {
+            rightOffset = new Vector2((coll.bounds.size.x/2) + coll.offset.x, coll.bounds.size.y / 2);
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+        }
+    }
+
     private void Update() 
     {
         Check();
@@ -29,12 +50,19 @@ public class PhysicsCheck : MonoBehaviour
     {
         // 检查是否碰撞地面
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+
+        // 墙体判断
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
     }
 
     private void OnDrawGizmosSelected()
     {
         // 绘制碰撞检测线, 方便调试
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
+        
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
     }
 
 }
